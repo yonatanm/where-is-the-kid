606e94eb7615af759c2b2fa941d5e579bcb4fefa
yonatanm <yonatan@outbrain.com>
2022-06-23 16:10:16 +0300

    test Thu 23 Jun 2022 16:10:16 IDT

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,3 +1,5 @@
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5000
 # nvm
 # https://gist.github.com/winuxue/cfef08e2f5fe9dfc16a1d67a4ad38a01
+
+PUPPETEER_PRODUCT=firefox npm i puppeteer
