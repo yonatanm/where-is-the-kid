75a65fe82215797674bb644909911883e1185248
yonatanm <yonatan@outbrain.com>
2022-07-14 15:55:38 +0300

    cleanup code

--- src/whatsapp/app.ts
+++ src/whatsapp/app.ts
@@ -171,25 +171,6 @@ const orchestrate = async (groupChat: GroupChat, message: Message) => {
     }
 }
 
-
-declare var window: any;// Window & typeof globalThis;
-
-// const getNumberIdPatch = async (number: string) => {
-//     if (!number.endsWith('@c.us')) {
-//         number += '@c.us';
-//     }
-//     const browser = waClient?.pupBrowser as any
-//     // console.log('browser', !!browser)
-
-//     return await waClient?.pupPage?.evaluate(async (number) => {
-//         const wid = window.Store.WidFactory.createWid(number);
-//         const result = await window.Store.QueryExist(wid);
-//         if (!result || result.wid === undefined) return null;
-//         return result.wid;
-//     }, number);
-
-// }
-
 const getStatus = async () => {
     let connected
     let { info } = waClient
